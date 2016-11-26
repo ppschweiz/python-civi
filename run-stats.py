48c4da5829e31104984e764d6fd49333a4a496fa
Stefan Thöni <stefan.thoeni@piatenpartei.ch>
2016-11-26 17:55:59 +0100

    Added statistics module

---
+++ run-stats.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Pirate Party Switzerland membership management script
+# Created by Stefan Thoeni at 2016-12-26
+#
+# Runs the stats process once
+#
+
+import sys
+import os
+from stats import process_stats
+
+process_stats();
+print("done");
---
+++ stats.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#
+# Pirate Party Switzerland membership management script
+# Created by Stefan Thoeni at 2016-11-26
+#
+# Computes member stats
+#
+
+import sys
+import os
+from pythoncivicrm.pythoncivicrm import CiviCRM
+from pythoncivicrm.pythoncivicrm import CivicrmError
+from pythoncivicrm.pythoncivicrm import matches_required
+from loader import load_all
+from sendemail import send_email
+from sendemail import notify_admin
+
+site_key = os.environ['CIVI_SITE_KEY']
+api_key = os.environ['CIVI_API_KEY']
+url = os.environ['CIVI_API_URL'] 
+civicrm = CiviCRM(url, site_key, api_key, True)
+
+def process_stats():
+	try:
+		members = load_all(civicrm, 1, 200)
+	
+		stats = dict();
+		for member in members:
+			try:
+				for membership in member.memberships:
+					name = membership.department.fullname
+					if not name in stats:
+        					stats[name] = type('Department', (), {})()
+        					stats[name].name = name
+        					stats[name].members = 0
+        					stats[name].voting = 0
+        					stats[name].verified = 0
+
+					stats[name].members += 1
+
+					if membership.active:
+						stats[name].voting += 1
+
+					if member.verified:
+						stats[name].verified += 1
+
+			except Exception as e:
+				msg = u'MemberId: {}\n{}\n{}'.format(member.member_id, type(e), e)
+				print(msg)
+				notify_admin(u'Error in handle_member', msg)
+
+		msg = u''
+		for name in stats:
+			line = u'{}: {} Mitglieder, {} Stimmberechtigte, {} Akkreditierte\n'.format(name, stats[name].members, stats[name].voting, stats[name].verified)
+			msg += line
+	
+		print(msg)	
+		send_email(u'info@piratenpartei.ch', u'stefan.thoeni@piratenpartei.ch', u'Mitgliederstatstik', msg.replace(u'\n', u'<br/>'), msg)
+
+	except Exception as e:
+		msg = u'{}\n{}'.format(type(e), e)
+		print(msg)
+		notify_admin(u'Error in process_factura', msg)
+
+
