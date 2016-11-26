a8fb9087e33cdc30c8c460406166e1364e78c6cd
Stefan Thöni <stefan.thoeni@piatenpartei.ch>
2016-11-26 20:45:28 +0100

    Fixed departments export

--- export_departments.py
+++ export_departments.py
@@ -15,17 +15,17 @@ def get_number(dep):
 
 deps = get_departments()
 
-sys.stdout.write('department,1.0\n')
-sys.stdout.write('id,parent,name\n')
+sys.stdout.write(u'department,1.0\n')
+sys.stdout.write(u'id,parent,name\n')
 depnums = list()
 
 for dep in sorted(deps.values(), key=get_number):
 	if not dep.number in depnums:
-		sys.stdout.write(str(dep.number) + ",")
+		sys.stdout.write(str(dep.number) + u',')
 		if dep.parent == None:
-			sys.stdout.write("None,")
+			sys.stdout.write(u'None,')
 		else:
-			sys.stdout.write(str(deps[dep.parent].fullname) + ",")
-		sys.stdout.write(dep.fullname + "\n")
+			sys.stdout.write(deps[dep.parent].fullname + u',')
+		sys.stdout.write(dep.fullname + u'\n')
 		depnums.append(dep.number)
 	
